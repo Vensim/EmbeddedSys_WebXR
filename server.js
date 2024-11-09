f0ed4ff4f8896a13c83c1331d43cda9fa8fabc9c
Vensim <nmeng@protonmail.com>
2024-11-09 04:19:20 +0000

    HTTPS self-cert

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -14,7 +16,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Bind to '0.0.0.0' to listen on all network interfaces
-app.listen(port, '0.0.0.0', () => {
-    console.log(`App running at http://0.0.0.0:${port}`);
+// WEBXR requires HTTPS, even for local
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+};
+
+https.createServer(options, app).listen(port, '0.0.0.0', () => {
+    console.log(`App running at https://192.168.1.xxx:${port}`);
 });
